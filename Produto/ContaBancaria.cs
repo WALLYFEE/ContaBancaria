389e08066125caed3906b6b3b6840d63ccdbc40a
WALLYFEE <wallyfeealves@gmail.com>
2023-02-28 10:51:05 -0300

    ContaBancaria

---
+++ Produto/ContaBancaria.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Produto
+{
+    internal class ContaBancaria
+    {
+        public string NomeTitular { get; set; }
+        public int NumeroConta { get; set; }
+        public double Saldo { get; set; }
+
+        public ContaBancaria()
+        {
+        }
+
+        public ContaBancaria(double saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+        public void Saque( double saque)
+        {
+            Saldo = Saldo - 5 - saque;
+        }
+
+        public override string ToString()
+        {
+            return "Titular: "
+                + NomeTitular
+                + ", Conta Nº"
+                + NumeroConta
+                + ", Saldo R$"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Produto/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace Produto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria();
+            ContaBancaria contaBancaria1 = new ContaBancaria(0.0);
+
+
+            Console.Write("Qual o seu nome: ");
+            contaBancaria.NomeTitular = Console.ReadLine();
+
+            Console.Write("Número conta: ");
+            contaBancaria.NumeroConta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Haverá depósito inicial: (S/N)");
+            string Resposta = Console.ReadLine();
+
+
+
+            if (Resposta == "S" || Resposta == "s")
+            {
+                Console.Write("Valor do depósito: ");
+                contaBancaria.Saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            else 
+            {
+                contaBancaria.Saldo = contaBancaria1.Saldo;
+            }
+
+            Console.WriteLine(contaBancaria);
+
+            Console.Write("Depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Deposito(quantia);
+
+            Console.WriteLine("Dados atualizados: " + contaBancaria);
+
+            Console.Write("Saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Saque(quantia);
+
+            Console.WriteLine("Dados atualizados: " + contaBancaria);
+
+
+        }
+    }
+}
